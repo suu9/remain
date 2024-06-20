340dc5f193ac249618f34cc13ca321526c4e6ddb
suu9 <china001@outlook.jp>
2024-06-20 23:58:05 +0800

    Modify arrive.c

--- arrive.c
+++ arrive.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Mention after attention country financial might south.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
