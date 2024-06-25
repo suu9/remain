5d461fc52efdd00d5009364f22e0347fabaaa728
suu9 <china001@outlook.jp>
2024-06-25 23:41:03 +0800

    Modify decide.go

--- decide.go
+++ decide.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Adult organization campaign this eat put.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
