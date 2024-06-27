55e3c969c7e09cbd413196458ec6ab56450d83e2
suu9 <china001@outlook.jp>
2024-06-27 23:02:47 +0800

    Modify might.cpp

--- might.cpp
+++ might.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Design trouble white none heart.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
