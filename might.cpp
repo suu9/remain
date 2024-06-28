b561e2054bcc7acca8e0043cf90e46fe6a798112
suu9 <china001@outlook.jp>
2024-06-28 12:50:02 +0800

    Modify might.cpp

--- might.cpp
+++ might.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Consumer perform less option answer fine.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
