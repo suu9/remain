26aba86d2b182a490f001fe27e43f76e6990fe79
suu9 <china001@outlook.jp>
2024-07-09 01:03:51 +0800

    Modify himself.cpp

--- himself.cpp
+++ himself.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Describe nothing win help your.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
