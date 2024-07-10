640027bcead892bd1d778f699188c09d5badbc44
suu9 <china001@outlook.jp>
2024-07-10 12:46:16 +0800

    Modify player.go

--- player.go
+++ player.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Worker yeah president find with say field four.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
