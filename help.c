3926f9c572c0e3b3187abb88cab85ac76a8adb2e
suu9 <china001@outlook.jp>
2024-08-01 12:32:07 +0800

    Modify help.c

--- help.c
+++ help.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Business necessary deal firm huge quickly half.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
