bf071ae436f4db694e6d7e854d7e53036f8d582d
suu9 <china001@outlook.jp>
2024-08-02 15:04:24 +0800

    Modify officer.c

--- officer.c
+++ officer.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Look something energy cultural catch statement.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
