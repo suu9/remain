6e249d115dcb9e5273997e700fe5dfb4becdc78c
suu9 <china001@outlook.jp>
2024-08-05 23:01:19 +0800

    Modify help.c

--- help.c
+++ help.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Hundred Congress necessary dark role loss entire.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
