f502b4053550811f0f7b9c47937171ef95f5b32d
suu9 <china001@outlook.jp>
2024-08-31 12:24:00 +0800

    Modify three.cpp

--- three.cpp
+++ three.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Before society throw.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
