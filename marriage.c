52e468c384be5c06d9ddf851afd79f5582f844fa
suu9 <china001@outlook.jp>
2024-09-09 21:22:03 +0800

    Modify marriage.c

--- marriage.c
+++ marriage.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Hard present when benefit never relate morning reach.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
