3cb3a3da25f7e9a65cf101177d8f0ac41374c123
suu9 <china001@outlook.jp>
2024-09-22 11:18:43 +0800

    Modify speech.cpp

--- speech.cpp
+++ speech.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sure down fire avoid option fly.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
