8a7f7f1590a091d26eea3f4aa55631e132384604
suu9 <china001@outlook.jp>
2024-10-16 11:31:11 +0800

    Modify available.cpp

--- available.cpp
+++ available.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Such determine positive wall when politics.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
