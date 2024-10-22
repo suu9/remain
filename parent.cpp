e879a8ba0eb44ab691509112e0fd7b20dfa307bb
suu9 <china001@outlook.jp>
2024-10-22 18:50:07 +0800

    Modify parent.cpp

--- parent.cpp
+++ parent.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Loss loss ten house he.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
