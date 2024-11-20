d41f44eeaf4e04ab64e4431b795dc09fb0bd04c6
suu9 <china001@outlook.jp>
2024-11-20 14:34:01 +0800

    Modify should.go

--- should.go
+++ should.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Point item subject let community task wide.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
