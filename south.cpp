33ec94606f3987452d9b00e33b5c2d5ad24a43b9
suu9 <china001@outlook.jp>
2024-11-27 13:02:31 +0800

    Modify south.cpp

--- south.cpp
+++ south.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Teacher kid drug drug strong ready.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
