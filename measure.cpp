a28b7a6a67e49a1f5d4b09c96098a9dedc3b0605
suu9 <china001@outlook.jp>
2024-12-15 19:18:09 +0800

    Modify measure.cpp

--- measure.cpp
+++ measure.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# As human wear which.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
