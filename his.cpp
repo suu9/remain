9f8b2a303ad14b593664ce89a9778ffc55d428f8
suu9 <china001@outlook.jp>
2025-01-15 12:16:37 +0800

    Modify his.cpp

--- his.cpp
+++ his.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mention degree budget year.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
